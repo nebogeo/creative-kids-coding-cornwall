46d808669541d687997c2819dc87b4576796341b
dave griffiths <dave@pawfal.org>
2014-04-05 18:59:12 +0000

    optimised block setting and made houses height procedural

--- python/dbscode_minecraft.py
+++ python/dbscode_minecraft.py
@@ -31,10 +31,14 @@ def write_block(blocktype,p):
 	mc.setBlock(p.x,p.y,p.z,blocktype)
 
 def box(t,pos,size):
-	for y in reversed(range(0,int(size.y))):
-		for z in range(0, int(size.z)):
-			for x in range(0, int(size.x)):
-				write_block(t,point(pos.x+x,pos.y+y,pos.z+z))
+	mc.setBlocks(pos.x,pos.y,pos.z,
+				pos.x+size.x-1,pos.y+size.y-1,
+				pos.z+size.z-1,
+				t)
+	#for y in reversed(range(0,int(size.y))):
+	#	for z in range(0, int(size.z)):
+	#		for x in range(0, int(size.x)):
+	#			write_block(t,point(pos.x+x,pos.y+y,pos.z+z))
 
 def sphere(t,pos,radius):
 	radius=int(radius)
@@ -83,17 +87,16 @@ def random_point(a,b):
 				 random.randrange(a.y,b.y),
 				 random.randrange(a.z,b.z))
 
+def choose(l):
+	return l[random.randint(0,len(l)-1)]
+
 def i_am_lost():
 	move_me_to(point(0,20,0))
 
 def bulldoze(size,height):
 	print("bulldozing")
-	for y in reversed(range(0,height)):
-		debug("bulldozing: "+str(y))
-		for z in range(-size/2, size/2):
-			for x in range(-size/2, size/2):
-				write_block(AIR,point(x,y,z))
-	box(STONE_BRICK,point(-size/2,-1,-size/2),point(size,1,size))
+	mc.setBlocks(-size/2,0,-size/2,size/2,height,size/2,AIR)
+	box(GRASS,point(-size/2,-1,-size/2),point(size,1,size))
 	print("finished bulldozing")
 
 
@@ -103,7 +106,7 @@ def planet(pos,size):
 	sphere(WOOD,pos,size*0.2)  
 	box(AIR,pos+point(0,-size,0),pos+point(size,size,size)) 
 
-#bulldoze(50,10)
+#bulldoze(200,200)
 
 #box(GOLD_BLOCK,point(20,0,0),point(4,4,4))
 #cone(GOLD_BLOCK,point(10,0,0),5,10)
@@ -111,19 +114,27 @@ def planet(pos,size):
 #cylinder(GOLD_BLOCK,point(-10,0,0),4,8)#
 #toblerone(GOLD_BLOCK,point(-24,0,0),point(8,8,4))
 
-def house(t,pos):
-	box(AIR,pos,point(10,12,10))
-	toblerone(t,pos+point(0,5,0),point(10,6,8))
-	toblerone(AIR,pos+point(0,3,0),point(10,6,8))
-	box(BRICK_BLOCK,pos+point(2,0,1),point(7,7,6))
-	box(BRICK_BLOCK,pos+point(4,7,1),point(3,2,6))
-	box(AIR,pos+point(3,0,2),point(5,7,4))
-	box(AIR,pos+point(4,0,6),point(3,4,1))
+def house(roof,walls,size,pos,flip):
+	box(AIR,pos,point(10,22,10))
+	toblerone(roof,pos+point(0,5+size,0),point(10,6,8))
+	toblerone(AIR,pos+point(0,3+size,0),point(10,6,8))
+	box(walls,pos+point(2,0,1),point(7,7+size,6))
+	box(walls,pos+point(4,7+size,1),point(3,2,6))
+	box(AIR,pos+point(3,0,2),point(5,7+size,4))
+	doorpos=6
+	if flip:
+		doorpos=1
+	box(AIR,pos+point(4,0,doorpos),point(3,4,1))
 
-house(MELON,point(-20,0,30))
-house(GOLD_BLOCK,point(-10,0,30))
-house(GLASS,point(0,0,30))
-house(OBSIDIAN,point(10,0,30))
+def street(pos,length,side):
+	for i in range(0,10):
+		house(choose([MELON,GOLD_BLOCK,WOOD,COBBLESTONE]),
+			choose([BRICK_BLOCK,COBBLESTONE,BEDROCK,SANDSTONE,LAPIS_LAZULI_BLOCK]),
+			random.randint(-3,5),pos+point(-40+i*10,0,-15),
+			side)
+
+street(point(0,0,0),6,True)
+street(point(0,0,-20),6,False)
 
 #toblerone(WOOD,point(30,0,0),point(20,10,10))
 #toblerone(AIR,point(30,-1,0),point(20,10,10))
