1a228633fb692e8bdb18e8a485a1c9d09b0c1fe6
dave griffiths <dave@pawfal.org>
2014-04-05 19:57:12 +0000

    fixed house

--- lessons/house.py
+++ lessons/house.py
@@ -1,18 +1,6 @@
 from dbscode_minecraft import *
 
-def planet(pos,size):
-	sphere(DIAMOND_BLOCK,pos,size)  
-	sphere(REDSTONE_ORE,pos,size*0.5)
-	sphere(WOOD,pos,size*0.2)  
-	box(AIR,pos+point(0,-size,0),pos+point(size,size,size)) 
-
-#bulldoze(200,200)
-
-#box(GOLD_BLOCK,point(20,0,0),point(4,4,4))
-#cone(GOLD_BLOCK,point(10,0,0),5,10)
-#sphere(GOLD_BLOCK,point(0,5,0),4)
-#cylinder(GOLD_BLOCK,point(-10,0,0),4,8)#
-#toblerone(GOLD_BLOCK,point(-24,0,0),point(8,8,4))
+bulldoze()
 
 def house(roof,walls,size,pos,flip):
 	box(AIR,pos,point(10,22,10))
@@ -35,12 +23,3 @@ def street(pos,length,side):
 street(point(0,0,0),6,True)
 street(point(0,0,-20),6,False)
 
-#toblerone(WOOD,point(30,0,0),point(20,10,10))
-#toblerone(AIR,point(30,-1,0),point(20,10,10))
-
-#i_am_lost()
-#cylinder(MELON,point(0,0,0),5,10)
-#cylinder(AIR,point(0,0,0),4,10)
-#cylinder(WATER,point(0,0,0),4,8)  
-
-#box(AIR,point(0,0,0),point(20,20,20))
